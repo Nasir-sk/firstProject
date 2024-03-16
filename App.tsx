b6c41158754b7ccecba504ad79345a65fe8e3bae
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-16 13:27:23 +0530

    simple form in React-Native

--- App.tsx
+++ App.tsx
@@ -2,16 +2,56 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet,  } from 'react-native';
 const App =()=>{
   const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [display, setDisplay] = useState(false);
+  
+  const resetForm=()=>{
+    setDisplay(false);
+    setEmail('');
+    setPassword('');
+    setName('');
+  }
   return(
     <View>
-      <Text style={{fontSize: 30}}>Handle Text Input</Text>
-      <Text style={{fontSize: 30}}>Your name is: {name}</Text>
-      <TextInput 
-        style={styles.textInput}
-        value={name}
-        onChangeText={(text)=>setName(text)}
+      <Text style={{fontSize: 30}}>Simple form in React Native</Text>
+        <TextInput 
+          style={styles.textInput}
+          placeholder='Enter user name'
+          value={name}
+          onChangeText={(text)=>setName(text)}
         />
-        <Button title='Clear Input Value' onPress={()=>setName('')}/>
+        <TextInput 
+          style={styles.textInput}
+          placeholder='Enter user email'
+          value={email}
+          onChangeText={(text)=>setEmail(text)}
+        />
+        <TextInput 
+          style={styles.textInput}
+          placeholder='Enter user password'
+          value={password}
+          onChangeText={(text)=>setPassword(text)}
+        />
+        <View style={{marginBottom: 10}}>
+        <Button 
+          title='Print details' 
+          color={'green'} 
+          onPress={()=>setDisplay(true)}
+        />
+        </View>
+        <Button title='clear datails' onPress={resetForm}/>
+        <View>
+          { 
+            display ?
+            <View>
+              <Text style={{fontSize: 30}}>User name is : {name}</Text>
+              <Text style={{fontSize: 30}}>User email is : {email}</Text>
+              <Text style={{fontSize: 30}}>User password is : {password}</Text>
+            </View>
+            : null
+          }
+        </View>
     </View>
   )
 }
