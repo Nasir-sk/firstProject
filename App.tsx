32e2b8bd11f2f49e06c6699bd34ac3b76a24bb6c
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-16 17:35:18 +0530

    Component in loop with FlatList

--- App.tsx
+++ App.tsx
@@ -1,60 +1,84 @@
 import React  from 'react';
-import { View, Text , StyleSheet } from 'react-native';
+import { View, Text , StyleSheet, FlatList } from 'react-native';
+import style from './style';
 const App =()=>{
   const users =[ 
     {
        id: 1,
-       name: "Naser"
+       name: "Naser",
+       email: 'nasir@gmail.com'
     },
     {
        id: 2,
-       name: "Shaikh"
+       name: "Shaikh",
+       email: 'nasir@gmail.com'
     },{
        id: 3,
-       name: "Johny"
+       name: "Johny",
+       email: 'nasir@gmail.com'
    },{
      id: 4,
-     name: "Depp"
+     name: "Depp",
+     email: 'nasir@gmail.com'
    },{
      id: 5,
-     name: "Kristen"
+     name: "Kristen",
+     email: 'nasir@gmail.com'
    },{
      id: 6,
-     name: "Stewert"
+     name: "Stewert",
+     email: 'nasir@gmail.com'
    },{
      id: 7,
-     name: "Prerna"
+     name: "Prerna",
+     email: 'nasir@gmail.com'
    },{
      id: 8,
-     name: "Salvi"
+     name: "Salvi",
+     email: 'nasir@gmail.com'
    },
    ]
   return(
     <View>
-     <Text style={{fontSize: 31}}>Grid with Dynamic Data</Text>
-    <View style={{flex:1, flexDirection:'row', flexWrap:'wrap'}}>
-      {
-        users.map((item)=><Text style={styles.item}>{item.name}</Text>)
-      }
-     </View>
+     <Text style={{fontSize: 31}}>Component in loop with FlatList</Text>
+      <FlatList 
+        data = {users}
+        renderItem={({item})=><UserData item={item}/>}/>
     </View>
     
   )
 }
 
+const UserData=(props: any)=>{
+  const item = props.item;
+  return(
+    <View style={styles.box}>
+      <Text style={styles.item}>{item.name}</Text>
+      <Text style={styles.item}>{item.email}</Text>
+      <Text style={styles.item}>{item.email}</Text>
+    </View>
+  )
+}
 const styles = StyleSheet.create({
   item:{
-    fontSize: 25,
-    padding: 5,
-    color: '#fff',
+    fontSize: 24,
+    flex: 1,
+    // padding: 5,
+    color: 'orange',
     // borderWidth: 1,
     backgroundColor:'blue',
     // borderColor: 'black',
-    margin: 5,
-    width: 120,
-    height: 120,
-    textAlignVertical:'center',
+    margin: 2,
+    // width: 120,
+    // height: 120,
+    // textAlignVertical:'center',
     textAlign: 'center'
+  },
+  box:{
+    flexDirection:'row',
+    borderWidth:2,
+    borderColor:'orange',
+    marginBottom: 10
   }
 })
 export default App;
