66fd12f541cec03229eda7b4003305bf0fc09508
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-16 16:59:44 +0530

    Grid with Dynamic Data

--- App.tsx
+++ App.tsx
@@ -1,49 +1,42 @@
 import React  from 'react';
-import { View, Text, StyleSheet, FlatList, ScrollView,  } from 'react-native';
+import { View, Text , StyleSheet } from 'react-native';
 const App =()=>{
- const users =[ 
- {
-    id: 1,
-    name: "Naser"
- },
- {
-    id: 2,
-    name: "Shaikh"
- },{
-    id: 3,
-    name: "Johny"
-},{
-  id: 4,
-  name: "Depp"
-},{
-  id: 5,
-  name: "Kristen"
-},{
-  id: 6,
-  name: "Stewert"
-},{
-  id: 7,
-  name: "Prerna"
-},{
-  id: 8,
-  name: "Salvi"
-},
-]
+  const users =[ 
+    {
+       id: 1,
+       name: "Naser"
+    },
+    {
+       id: 2,
+       name: "Shaikh"
+    },{
+       id: 3,
+       name: "Johny"
+   },{
+     id: 4,
+     name: "Depp"
+   },{
+     id: 5,
+     name: "Kristen"
+   },{
+     id: 6,
+     name: "Stewert"
+   },{
+     id: 7,
+     name: "Prerna"
+   },{
+     id: 8,
+     name: "Salvi"
+   },
+   ]
   return(
     <View>
-      <Text style={{fontSize: 31}}>List with Flatlist</Text>
-      <ScrollView style={{marginBottom: 40}}>
-      <FlatList
-        data={users}
-        renderItem={({item})=><Text style={styles.item}>{item.name}</Text>}
-        keyExtractor={item=>item.id}
-        />
-        {
-          users.map((item)=>
-          <Text style={styles.item}>{item.name}</Text>
-          )
-        }
-        </ScrollView>
+     <Text style={{fontSize: 31}}>Grid with Dynamic Data</Text>
+    <View style={{flex:1, flexDirection:'row', flexWrap:'wrap'}}>
+      {
+        users.map((item)=><Text style={styles.item}>{item.name}</Text>)
+      }
+     </View>
     </View>
     
   )
@@ -51,13 +44,17 @@ const App =()=>{
 
 const styles = StyleSheet.create({
   item:{
-    fontSize: 20,
-    padding: 10,
+    fontSize: 25,
+    padding: 5,
     color: '#fff',
-    borderWidth: 1,
+    // borderWidth: 1,
     backgroundColor:'blue',
-    borderColor: 'black',
-    margin: 10
+    // borderColor: 'black',
+    margin: 5,
+    width: 120,
+    height: 120,
+    textAlignVertical:'center',
+    textAlign: 'center'
   }
 })
 export default App;
