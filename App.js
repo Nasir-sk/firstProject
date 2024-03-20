240cb7cbdc102eae4eb3861f7219fc152fb6ebaf
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-20 16:15:38 +0530

    style with button in React-Native

--- App.js
+++ App.js
@@ -1,51 +1,30 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
 const App =()=>{
   return(
     <View style={styles.main}>
-      <View style={styles.box1}>
-        <View style={styles.InnerBox1}></View>
-        <View style={styles.InnerBox2}></View>
-        <View style={styles.InnerBox3}></View>
-      </View>
-      <View style={styles.box2}></View>
-      <View style={styles.box3}></View>
+      <TouchableOpacity>
+        <Text style={styles.button}>Button</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  main:{
-    flex:1,
-    backgroundColor:'green'
+  main: {
+    flex: 1
   },
-  box1:{
-    flex: 2,
-    backgroundColor:'red',
-    flexDirection:'row'
-  },
-  box2:{
-    flex: 2,
-    backgroundColor:'green'
-  },
-  box3:{
-    flex: 1,
-    backgroundColor:'black'
-  },
-  InnerBox1:{
-    flex: 1,
-    backgroundColor:'skyblue',
-    margin: 10
-  },
-   InnerBox2:{
-    flex: 1,
-    backgroundColor:'skyblue',
-    margin: 10
-  },   
-  InnerBox3:{
-    flex: 1,
-    backgroundColor:'skyblue',
-    margin: 10
-  },
+  button:{
+    backgroundColor:'#bbb',
+    color: '#fff',
+    fontSize:24,
+    textAlign: 'center',
+    padding: 10,
+    margin: 10,
+    shadowColor: 'red',
+    elevation: 10,
+    borderRadius: 10,
+    shadowOpacity: 1
+  }
 })
 export default App;
