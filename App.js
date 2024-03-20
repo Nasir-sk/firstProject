ea9b3e5e8dd985f2fc1d59f3d7089c91a35c22c5
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-20 15:44:33 +0530

    ComponentDidMount

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
-import React, {  useState }  from 'react';
+import React, { useEffect, useState }  from 'react';
 import { View, Text , Button } from 'react-native';
 const App =()=>{
-  const [show, setShow] = useState(true);
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(100);
+
+  useEffect(()=>{
+    console.warn('do some animation')
+  }, [count]);
+  
+  useEffect(()=>{
+    console.warn('call some api here')
+  }, [data]);
+  
 
   return(
     <View>
-     <Text> Hide and Show component</Text>
-     <Button title='Show' onPress={()=>setShow(!show)}/>
-     {
-      show ? <User/> : null
-     }
+     <Text>ComponentDidMount</Text>
+     <Button title='update count' onPress={()=>setCount(count+1)}/>
+     <Button title='update data' onPress={()=>setData(data+1)}/>
+     <User info={{count, data}}/>
+     
     </View>
   )
 }
 
-const User=()=>{
+const User=(props)=>{
+  console.warn(props.info);
+  useEffect(()=>{
+    console.warn("run this code when prop is updated");
+  },[props.info.count, props.info.data])
   return(
     <View>
-      <Text>User Component</Text>
+      <Text>User Component {props.info.count}</Text>
     </View>
   )
 }
