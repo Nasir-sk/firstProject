765c1671544503a38c8be97babfcb73774090daf
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-20 16:05:56 +0530

    Responsive layout with flex

--- App.js
+++ App.js
@@ -1,34 +1,51 @@
-import React, { useEffect, useState }  from 'react';
-import { View, Text , Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 const App =()=>{
-  const [show, setShow] = useState(true);
-
   return(
-    <View>
-     <Text>useEffect for unMount Component</Text>
-     <Button title='Hide and Show' onPress={()=>setShow(!show)}/>
-    {
-      show ?
-      <User/>: null
-     }
-     
+    <View style={styles.main}>
+      <View style={styles.box1}>
+        <View style={styles.InnerBox1}></View>
+        <View style={styles.InnerBox2}></View>
+        <View style={styles.InnerBox3}></View>
+      </View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
     </View>
   )
 }
 
-const User=(props)=>{
-  const timer = setInterval(()=>{
-    console.warn("Timer called");
-  }, 2000)
-
-  useEffect(()=>{
-    return ()=> clearInterval(timer)
-  })
-  return(
-    <View>
-      <Text>User Component</Text>
-    </View>
-  )
-}
-//componentDidMount for a specific component execute
+const styles = StyleSheet.create({
+  main:{
+    flex:1,
+    backgroundColor:'green'
+  },
+  box1:{
+    flex: 2,
+    backgroundColor:'red',
+    flexDirection:'row'
+  },
+  box2:{
+    flex: 2,
+    backgroundColor:'green'
+  },
+  box3:{
+    flex: 1,
+    backgroundColor:'black'
+  },
+  InnerBox1:{
+    flex: 1,
+    backgroundColor:'skyblue',
+    margin: 10
+  },
+   InnerBox2:{
+    flex: 1,
+    backgroundColor:'skyblue',
+    margin: 10
+  },   
+  InnerBox3:{
+    flex: 1,
+    backgroundColor:'skyblue',
+    margin: 10
+  },
+})
 export default App;
