a06961a7519c58b3baa75849b77b9ed2fbbf267d
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-20 16:31:12 +0530

    2 TouchableOpacity  with text

--- App.js
+++ App.js
@@ -1,30 +1,63 @@
-import React from 'react';
-import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, TouchableOpacity, StyleSheet , } from 'react-native';
 const App =()=>{
+  const [selectedRadio, setSelectedRaddio] = useState(1);
+
   return(
     <View style={styles.main}>
-      <TouchableOpacity>
-        <Text style={styles.button}>Button</Text>
+      <TouchableOpacity onPress={()=>{setSelectedRaddio(1)}}>
+        <View style={styles.radioWrapper}>
+          <View style={styles.radio}>
+            {
+              selectedRadio === 1 ? <View style={styles.radioBg}></View>: null
+            }
+          </View>
+          <Text style={styles.radioText}>Radio 1</Text>
+        </View>
       </TouchableOpacity>
+      <TouchableOpacity onPress={()=>{setSelectedRaddio(2)}}>
+        <View style={styles.radioWrapper}>
+          <View style={styles.radio}>
+            {
+              selectedRadio === 2 ? <View style={styles.radioBg}></View>: null
+            }
+          </View>
+          <Text style={styles.radioText}>Radio 1</Text>
+        </View>
+      </TouchableOpacity>
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  main: {
-    flex: 1
+  main:{
+    flex: 1,
+    alignItems:'center',
+    justifyContent:'center'
   },
-  button:{
-    backgroundColor:'#bbb',
-    color: '#fff',
-    fontSize:24,
-    textAlign: 'center',
-    padding: 10,
-    margin: 10,
-    shadowColor: 'red',
-    elevation: 10,
-    borderRadius: 10,
-    shadowOpacity: 1
+  radioText:{
+    fontSize: 20,
+    color: 'skyblue'
+  },
+  radio:{
+    height: 40,
+    width: 40,
+    borderColor: 'blue',
+    borderWidth: 2,
+    borderRadius: 20,
+     margin: 10
+  },
+  radioBg:{
+    backgroundColor:'skyblue',
+    height: 28,
+    width: 28,
+    borderRadius: 20,
+    margin: 4
+  },
+  radioWrapper:{
+    flexDirection: 'row',
+    alignItems: 'center'
   }
 })
 export default App;
