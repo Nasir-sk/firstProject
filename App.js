7ff5da3a86ed946ff670aa50a8baf473a54f3040
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-20 15:15:55 +0530

    Life cycle with useEffect

--- App.js
+++ App.js
@@ -1,38 +1,18 @@
-import React  from 'react';
-import { View, Text , SectionList } from 'react-native';
+import React, { useEffect, useState }  from 'react';
+import { View, Text , SectionList, Button } from 'react-native';
 const App =()=>{
-  
-  const users=[
-    {
-      id: 1,
-      name: "Naser",
-      data:["php", "React js", "Angular"]
-    },
-    {
-      id: 2,
-      name: "Peter",
-      data:["Java", "JS", "HTML"]
-    },{
-      id: 3,
-      name:"Tony",
-      data:["CSS","BootsTrap", "Python"]
-    },{
-      id: 4,
-      name:"Tony",
-      data:["CSS", "Bootstrap", "HTML"]
-    }
-  ]
+  const [count, setCount] = useState(0);
+
+  useEffect(()=>{
+    console.warn(count)
+  },[count])
   return(
     <View>
-     <Text style={{fontSize: 31}}>SectionList to display nestedd array</Text>
-     <SectionList 
-     sections={users}
-     renderItem={({item})=><Text style={{fontSize: 20, marginLeft: 20}}>{item}</Text>}
-     renderSectionHeader={({section:{name}})=>(
-      <Text style={{fontSize: 25, color:'red'}}>{name}</Text>
-  )}
-     />
+     <Text style={{fontSize: 30}}>Life cycle with useEffect{count}</Text>
+     <Button title='update count' onPress={()=>setCount(count+1)}/>
     </View>
   )
 }
+
+//componentDidMount for a specific component execute
 export default App;
