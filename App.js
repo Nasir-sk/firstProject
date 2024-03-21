d189d0cd0dc0ac713af7915a0741142222e1beca
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-21 20:43:24 +0530

    Custom Modal(Pop-up)

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import {   WebView  } from 'react-native-webview';
+import React, { useState } from 'react';
+import {   Button, StyleSheet, View, Text  } from 'react-native';
 const App =()=>{
- 
+ const [show, setShow] = useState(false);
   return(
-   <WebView source={{uri:"https:www.facebook.com"}}/>
+    <View style={styles.container}>
+      {
+        show?
+        <View style={styles.modal}>
+          <View style={styles.body}>
+            <Text>Some Text</Text>
+            <Button title='close' onPress={()=>setShow(false)}/>
+          </View>
+        </View>
+        :null
+      }
+     <Button title='Open Dialog' onPress={()=>setShow(true)}/>
+    </View>
   )
 }
 
-
+const styles = StyleSheet.create({
+  container:{
+  flex:1,
+  justifyContent:'flex-end'
+  },
+  modal:{
+    flex: 1,
+    backgroundColor:'rgba(50,50,50,.5)',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  body:{
+    backgroundColor:'#fff',
+    height:300,
+    width:300,
+    padding:20,
+    justifyContent:'flex-end',
+    borderRadius:10
+  }
+})
 export default App;
