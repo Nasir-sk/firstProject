ea4f5bc230e012269a1a234837a60d1c68d1a389
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-21 19:38:40 +0530

    ActivityOndicator(Loader)

--- App.js
+++ App.js
@@ -1,31 +1,20 @@
 import React, {useState} from 'react';
-import { View, Text, TouchableOpacity, StyleSheet , } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, Button , } from 'react-native';
 const App =()=>{
-  const [selectedRadio, setSelectedRaddio] = useState(1);
-
+  const [show, setShow] = useState(false);
+  const displayLoader=()=>{
+    setShow(true);
+    setTimeout(()=>{
+      setShow(false)
+    }, 3000);
+  }
   return(
     <View style={styles.main}>
-      <TouchableOpacity onPress={()=>{setSelectedRaddio(1)}}>
-        <View style={styles.radioWrapper}>
-          <View style={styles.radio}>
-            {
-              selectedRadio === 1 ? <View style={styles.radioBg}></View>: null
-            }
-          </View>
-          <Text style={styles.radioText}>Radio 1</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={()=>{setSelectedRaddio(2)}}>
-        <View style={styles.radioWrapper}>
-          <View style={styles.radio}>
-            {
-              selectedRadio === 2 ? <View style={styles.radioBg}></View>: null
-            }
-          </View>
-          <Text style={styles.radioText}>Radio 1</Text>
-        </View>
-      </TouchableOpacity>
-
+     {
+      show ?
+      <ActivityIndicator size={100} color={'green'} aminating={show} />: null
+     }
+     <Button title='show loader' onPress={displayLoader}/>
     </View>
   )
 }
@@ -35,29 +24,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems:'center',
     justifyContent:'center'
-  },
-  radioText:{
-    fontSize: 20,
-    color: 'skyblue'
-  },
-  radio:{
-    height: 40,
-    width: 40,
-    borderColor: 'blue',
-    borderWidth: 2,
-    borderRadius: 20,
-     margin: 10
-  },
-  radioBg:{
-    backgroundColor:'skyblue',
-    height: 28,
-    width: 28,
-    borderRadius: 20,
-    margin: 4
-  },
-  radioWrapper:{
-    flexDirection: 'row',
-    alignItems: 'center'
   }
 })
 export default App;
