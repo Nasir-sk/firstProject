51e943383567e84f353599a27f20093f23271d03
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-21 19:52:13 +0530

    Modal (Dialog Box)

--- App.js
+++ App.js
@@ -1,29 +1,54 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet, ActivityIndicator, Button , } from 'react-native';
+import { View, Text, StyleSheet, Button, Modal , } from 'react-native';
 const App =()=>{
-  const [show, setShow] = useState(false);
-  const displayLoader=()=>{
-    setShow(true);
-    setTimeout(()=>{
-      setShow(false)
-    }, 3000);
-  }
+  const [showModal, setShowModal] = useState(false);
   return(
     <View style={styles.main}>
-     {
-      show ?
-      <ActivityIndicator size={100} color={'green'} aminating={show} />: null
-     }
-     <Button title='show loader' onPress={displayLoader}/>
+     <Modal
+      transparent={true}
+      visible={showModal}
+      animationType='slide'
+     >
+     <View style={styles.centeredView}> 
+     <View style={styles.modalView}>
+      <Text style={styles.modalText}>
+        Hello code step by step
+      </Text>
+      <Button title='clode modal' onPress={()=>setShowModal(false)}/>
+     </View>
+     </View>
+     </Modal>
+     <View style={styles.buttonView}>
+      <Button title='Open Modal' onPress={()=> setShowModal(true)} />
+     </View>
     </View>
+    
   )
 }
 
 const styles = StyleSheet.create({
   main:{
     flex: 1,
+  },
+  buttonView:{
+    flex: 1,
+    justifyContent:'center'
+  },
+  centeredView:{
+    flex:1,
     alignItems:'center',
     justifyContent:'center'
+  },
+  modalView:{
+    backgroundColor:'#fff',
+    padding:30,
+    borderRadius:20,
+    shadowColor:'black',
+    elevation:5
+  },
+  modalText:{
+    fontSize: 30,
+    marginBottom: 20
   }
 })
 export default App;
