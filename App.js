d94862b3e0c73b9b24ab8b2e7a8656261062e381
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-21 20:33:02 +0530

    WebView npm Installed and check

--- App.js
+++ App.js
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
-import {   Platform, StyleSheet, View, Text  } from 'react-native';
+import React from 'react';
+import {   WebView  } from 'react-native-webview';
 const App =()=>{
  
   return(
-    <View style={styles.container}>
-     <Text style={{fontSize:30}}>Platform:{Platform.OS}</Text>
-     {
-      Platform.OS == 'android'?
-      <View style={{backgroundColor:'green', height: 100, width:100}}></View>
-      :
-      <View style={{backgroundColor:'red', height: 100, width:100}}></View>
-     }
-     <Text style={styles.text}>Hello</Text>
-     <Text>{JSON.stringify(Platform.constants.reactNativeVersion)}</Text>
-    </View>
+   <WebView source={{uri:"https:www.facebook.com"}}/>
   )
 }
 
-const styles = StyleSheet.create({
-  text:{
-    color:Platform.OS=='android'?"orange":'blue',
-    fontSize:20
-  }
-})
+
 export default App;
