957196a7fd4e865d966bd0dbd64891617768d495
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-30 13:22:58 +0530

    simple form validation & POST api with input field

--- App.js
+++ App.js
@@ -1,33 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import {   View, Text } from 'react-native';
+import React, {  useState } from 'react';
+import {   View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const App =()=>{
-  const [data, setData] = useState([]);
- const getAPIData = async ()=>{
+ const [name, setName] = useState('');
+ const [email, setEmail] = useState('');
+ const [age, setAge] = useState(0);
+
+ const [nameErr, setNameErr] = useState(false);
+ const [emailErr, setEmailErr] = useState(false);
+ const [ageErr, setAgeErr] = useState(false);
+
+ saveData = async () =>{
+
+  if(!name){
+    setNameErr(true);
+  }else{
+    setNameErr(false)
+  }
+  if(!email){
+    setEmailErr(true);
+  }else{
+    setEmailErr(false)
+  }
+  if(!age){
+    setAgeErr(true);
+  }else{
+    setAgeErr(false)
+  }
+
+  if(!name || !email || !age){
+    return false;
+  }
   const url = "http://10.0.2.2:3000/users";
-  let result = await fetch(url);
+  let result = await fetch(url,{
+    method: "Post",
+    body: JSON.stringify({name, email, age}),
+    headers:{ "Content-Type":"application/json"}
+  })
   result = await result.json();
-  setData(result)
+  console.warn(result);
  }
- useEffect(()=>{
-  getAPIData();
- })
+
   return(
     <View>
-      <Text style={{fontSize:30,}}>FlatList API Call</Text>
-    { data.length ? 
-      data.map((item)=><View  style={{borderColor:"black", borderWidth:2}}>
-     
-      <Text style={{fontSize:20}}>{item.id}</Text>
-      <Text style={{fontSize:20}}>{item.namw}</Text>
-      <Text style={{fontSize:20}}>{item.Email}</Text>
-      <Text style={{fontSize:20}}>{item.age}</Text>
-    </View>
-    )
-       :null
-    }
+     <TextInput style={Styles.input} value={name} onChangeText={(text)=>setName(text)} placeholder='Enter Name'/>
+     { nameErr ? <Text style={Styles.inputErr}>Please enter valid name</Text> : null}
+     <TextInput style={Styles.input} value={email} onChangeText={(text)=>setEmail(text)} placeholder='Enter Email'/>
+     { emailErr ? <Text style={Styles.inputErr}>Please enter valid email</Text> : null}
+     <TextInput style={Styles.input} value={age} onChangeText={(text)=>setAge(text)} placeholder='Enter Age'/>
+     { ageErr ? <Text style={Styles.inputErr}>Please enter valid age</Text> : null}
+     <Button title='save data' onPress={saveData}/>
     </View>
     )
 }
+
+const Styles = StyleSheet.create({
+  input:{
+    borderColor:"blue",
+    borderWidth:1,
+    margin:4,
+    borderRadius:4,
+    textAlign: "justify"
+  },
+  inputErr:{
+    margin: 3,
+    color: "red",
+    marginLeft: 6
+  }
+})
 
 export default App;
