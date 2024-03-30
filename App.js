0e00e69f8eebd9cd62a25bec37f13e68221a7fb0
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-30 15:11:47 +0530

    create Modal to update data

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, {  useEffect, useState } from 'react';
-import {   View, Text,  Button, StyleSheet } from 'react-native';
+import {   View, Text,  Button, StyleSheet, Modal } from 'react-native';
 
 const App =()=>{
  const [data, setData]=useState([])
-
+ const [showModal, setShowModal] = useState(false);
+ const [selectedUser, setSelectedUser]= useState(undefined)
  const getAPIData = async ()=>{
   const url = "http://10.0.2.2:3000/users";
   let result = await fetch(url);
@@ -27,6 +28,11 @@ const App =()=>{
 useEffect(()=>{
   getAPIData();
 },[])
+
+const  updateUser =(data)=>{
+  setShowModal(true);
+  setSelectedUser(data)
+}
   return(
     <View style={styles.container}>
       <View style={styles.dataWrapper}> 
@@ -53,17 +59,31 @@ useEffect(()=>{
               <Button title='delete' onPress={()=>deleteAPI(item.id)}/>
             </View>
             <View style={{flex: 1}}>
-              <Button title='update'/>
+              <Button title='update' onPress={()=>updateUser(item)}/>
             </View>
             
           </View>)
           :null
         }
+        <Modal visible={showModal} transparent={true}>
+         <UserModal setShowModal={setShowModal} selectedUser={selectedUser}/>
+        </Modal>
     
     </View>
     )
 }
 
+const UserModal =(props)=>{
+  console.warn(props.selectedUser);
+  return(
+    <View style={styles.centeredView}>
+    <View style={styles.modalView}>
+      <Text>{props.selectedUser.name}</Text>
+      <Button title='Close' onPress={()=>props.setShowModal(false)}/>
+    </View>
+  </View>
+  )
+}
 const styles = StyleSheet.create({
   container:{
     flex:1
@@ -74,6 +94,19 @@ const styles = StyleSheet.create({
     backgroundColor:"orange",
     margin:5,
     padding: 8
+  },
+  centeredView:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:"center"
+  },
+  modalView:{
+    backgroundColor:"#fff",
+    padding: 40,
+    borderRadius:10,
+    shadowColor:"#000",
+    shadowOpacity:0.70,
+    elevation:5
   }
 })
 
