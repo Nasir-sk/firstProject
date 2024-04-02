41ec71949b19854ea72258683a3657b5804b10fe
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-04-02 22:53:02 +0530

    call api in saga and render data

--- components/ProductWrapper.js
+++ components/ProductWrapper.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { StyleSheet, Text, View,  ScrollView, Button } from 'react-native'
 import Header from './Header'
 import Product from './Product'
-import UserList from './UserList'
 
 const ProductWrapper=({navigation})=>{
   const products =[
--- components/UserList.js
+++ components/UserList.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
+import {getUserList} from './redux/action';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function UserList() {
+  const dispatch = useDispatch();
+  const userList = useSelector((state)=>state.reducer)
+  useEffect(()=>{
+    dispatch(getUserList());
+  },[])
+  
+  console.warn("in component", userList);
   return (
     <View style={styles.container}>
-        <Text>UserList</Text>
+        {
+          userList.length?
+          userList.map((item)=>(
+            <Text style={{fontSize:18}}>{item.firsName}</Text>
+          ))
+          :null
+        }
     </View>
   )
 }
