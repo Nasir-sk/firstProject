0feb4959c0f7b1ab205419954706f9d73ff0fd51
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-04-02 11:23:08 +0530

    remove from cart UI

--- components/Product.js
+++ components/Product.js
@@ -1,25 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, Image, Button, View } from 'react-native'
 
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import { addToCart} from './redux/action';
 
 
 const Product=(props)=>{
     const item = props.item;
-
     const dispatch = useDispatch();
+    const cartItem = useSelector((state)=>state.reducer);
+    const [isAdded, setIsAdded]=useState(false);
 
     const handleAddToCart=(item)=>{
         dispatch(addToCart(item))
     }
+
+    useEffect(()=>{
+        if(cartItem && cartItem.length){
+            cartItem.forEach((element)=>{
+                if(element.name === item.name){
+                    setIsAdded(true)
+                }
+            })
+        }
+    },[cartItem])
     return(
         <View style={styles.cotainer}>
               <Text style={{fontSize:24}}>{item.name}</Text>
               <Text style={{fontSize:24}}>{item.price}</Text>
               <Text style={{fontSize:24}}>{item.color}</Text>
               <Image style={{width:200, height: 200, borderRadius:5,borderColor: "black", borderWidth:5, marginBottom:5 }} source={{uri:item.image}}/>
-              <Button title='add to cart' onPress={()=>handleAddToCart(item)}/>
+              {
+                isAdded?
+                <Button title='remove to cart' onPress={()=>handleAddToCart(item)}/>
+                :
+                <Button title='add to cart' onPress={()=>handleAddToCart(item)}/>
+              }
               </View>
     )
 }
