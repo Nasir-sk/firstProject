0e3a6f81ff47afdb2d8b7717b90bf7f499f6e23a
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-04-02 10:51:38 +0530

    get redux data

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-
+import {  useSelector } from 'react-redux'
 const Header=()=>{
+    const cartData = useSelector((state)=>state.reducer);
+    const [cartItem, setCartItem] = useState(0)
+
+    useEffect(()=>{
+        setCartItem(cartData.length)
+    }, [cartData])
     return(
-        <View>
-            <Text style={{fontSize: 30, textAlign:"right", padding:10, backgroundColor:"orange"}}>0</Text>
+        <View style={styles.container}>
+            <Text style={{fontSize: 30, textAlign:"right",padding:20, backgroundColor:"orange"}}>
+                <View style={{backgroundColor:"yellow", borderRadius:15,height:40, width:40}}>
+                    <Text style={{fontSize:30}}>{cartItem}</Text>
+                </View>
+            </Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    container:{
+        flex:1
+    }
 })
 
 export default Header;
